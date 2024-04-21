a8cb3087c0bc570361368d844680a0e7ef57be8c
wildan <21523222@students.uii.ac.id>
2024-04-21 21:37:42 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const message = (nama) => {
+//     console.log(`Morning ${nama}`);
+// }
+
+// message('world!');
+
+const cpuInformation = process.memoryUsage();
+ 
+console.log(cpuInformation);
---
+++ process-object/index.js
@@ -0,0 +1,16 @@
+// Import the 'process' module if not using CommonJS module syntax
+// const process = require('process');
+
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for(let i = 0; i <= 10000; i++) {
+    // This looping process will increase memory usage
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
